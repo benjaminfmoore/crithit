d78b5eba3fd97dec8b4fb576a3197d94037b8450
benjaminfmoore <benjaminfmoore@yahoo.com>
2020-07-03 19:37:59 -0400

    edited about me page. need to work on making it flashier

--- src/pages/aboutme/AboutMe.jsx
+++ src/pages/aboutme/AboutMe.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "../../index.css";
+import "./AboutMe.css";
 import profilepic from "../../images/profilepic.jpeg";
 
 function AboutMe() {
   return (
     <>
-      <div class="aboutMeContainer" alt="about me container">
-        <h2>ABOUT ME</h2>
-        <div id="about-crithit" alt="about the company">
+      <div className="aboutMeContainer" alt="about me container">
+        <h2>ABOUT THE COMPANY</h2>
+        <div className="about-crithit" alt="about the company">
           <p>
             Crit Hit WebDev is a freelance company created by Benjamin Moore IV.
             It is web development for gamers designed by gamers. Our goal is to
@@ -18,19 +19,29 @@ function AboutMe() {
 
         <div class="break"></div>
 
-        <div id="aboutBenjamin" alt="about-benjamin">
-          <img src={profilepic} alt="Benjamin" height="200px" width="170px" />
+        <div className="about-benjamin" alt="about benjamin">
+          <h2>ABOUT ME</h2>
+          <img
+            className="profile-pic"
+            src={profilepic}
+            alt="Benjamin"
+            height="200px"
+            width="170px"
+          />
 
           <p>
             Benjamin Moore is still working on his level 5 wizard beard. A lover
             of all things RPG oriented, Benjamin has a drawer full of dice to
             settle disputes. Knowing he has a terrible sense of aiming, Benjamin
             prefers to take the roles of support, tanking his way to a team
-            victory. A recent graduate from Wyncode Academy, Benjamin studied
-            React, Javascript, HTML/CSS, and is very proficient in databases
-            (his work using MongoDB helped win his team the best app award
-            during pitch night). When he's not turning coffee into code, You can
-            either find him live-streaming his shenanigans on{" "}
+            victory.
+          </p>
+          <p>
+            A recent graduate from Wyncode Academy, Benjamin studied React,
+            Javascript, HTML/CSS, and is very proficient in databases (his work
+            using MongoDB helped win his team the best app award during pitch
+            night). When he's not turning coffee into code, You can either find
+            him live-streaming his shenanigans on{" "}
             <a href="https://twitch.tv/m00simus">his twitch channel.</a> or
             spending time with his fiancé and his German Shepherd co-pilot
             Chewie
